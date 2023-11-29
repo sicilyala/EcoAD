7fdb2a14afe13401f29a769c7a67795231d461ca
cwq <cwq@cwqaq.laptop>
2023-11-29 16:11:39 +0800

    changed highway-env, ready for training

---
+++ highway_env/envs/ecoad_env.py
@@ -0,0 +1,136 @@
+from typing import Dict, Text
+
+import numpy as np
+import os
+
+from highway_env import utils
+from highway_env.envs.common.abstract import AbstractEnv
+from highway_env.envs.common.action import Action
+from highway_env.road.road import Road, RoadNetwork
+from highway_env.utils import near_split
+from highway_env.vehicle.controller import ControlledVehicle
+from highway_env.vehicle.kinematics import Vehicle
+
+
+class EcoADEnv(AbstractEnv):
+    name = 'EcoADEnv'
+    """
+    An ecological autonomous driving (ECO-AD) environment in highway scenario.
+
+    The vehicle is driving on a straight highway with several lanes
+    """
+
+    @classmethod
+    def default_config(cls) -> dict:
+        # print("in ecoad_env.py default_config, L24")
+        # print(super())
+        # print(EcoADEnv.mro())
+        # print('\n***** main.py is in: %s *****' % os.getcwd())
+        # print('\n***** The env is in: highway_env.envs:EcoADEnv *****\n')
+
+        de_config = super().default_config()  # a dict
+        de_config.update({
+            "observation": {
+                "type": "Kinematics"
+            },
+            "action": {
+                "type": "DiscreteMetaAction",
+            },
+            "lanes_count": 4,
+            "vehicles_count": 50,
+            "controlled_vehicles": 1,
+            "initial_lane_id": None,
+            "duration": 40,  # [s]
+            "ego_spacing": 2,
+            "vehicles_density": 1,
+            "collision_reward": -1,  # The reward received when colliding with a vehicle.
+            "right_lane_reward": 0.1,  # The reward received when driving on the right-most lanes, linearly mapped to
+            # zero for other lanes.
+            "high_speed_reward": 0.4,  # The reward received when driving at full speed, linearly mapped to zero for
+            # lower speeds according to config["reward_speed_range"].
+            "lane_change_reward": 0,  # The reward received at each lane change action.
+            "reward_speed_range": [20, 30],
+            "normalize_reward": True,
+            "offroad_terminal": False
+        })
+        return de_config
+
+    def _reset(self) -> None:
+        self._create_road()
+        self._create_vehicles()
+        self.vehicle.AgentEMS.reset_obs()
+        # print("EcoADEnv: _reset(): AgentEMS.reset_obs()")
+
+    def _create_road(self) -> None:
+        """Create a road composed of straight adjacent lanes."""
+        self.road = Road(network=RoadNetwork.straight_road_network(self.config["lanes_count"], speed_limit=30),
+                         np_random=self.np_random, record_history=self.config["show_trajectories"])
+
+    def _create_vehicles(self) -> None:
+        """Create some new random vehicles of a given type, and add them on the road."""
+        other_vehicles_type = utils.class_from_path(self.config["other_vehicles_type"])
+        # "highway_env.vehicle.behavior.IDMVehicle"
+        other_per_controlled = near_split(self.config["vehicles_count"], num_bins=self.config["controlled_vehicles"])
+
+        self.controlled_vehicles = []
+        for others in other_per_controlled:
+            vehicle = Vehicle.create_random(
+                self.road,
+                speed=25,
+                lane_id=self.config["initial_lane_id"],
+                spacing=self.config["ego_spacing"]
+            )
+            vehicle = self.action_type.vehicle_class(self.road, vehicle.position, vehicle.heading, vehicle.speed)
+            self.controlled_vehicles.append(vehicle)
+            self.road.vehicles.append(vehicle)
+
+            for _ in range(others):
+                vehicle = other_vehicles_type.create_random(self.road, spacing=1 / self.config["vehicles_density"])
+                vehicle.randomize_behavior()
+                self.road.vehicles.append(vehicle)
+
+    def _reward(self, action: Action) -> float:
+        """
+        The reward is defined to foster driving at high speed, on the rightmost lanes, and to avoid collisions.
+        :param action: the last action performed
+        :return: the corresponding reward
+        """
+        rewards = self._rewards(action)
+        reward = sum(self.config.get(name, 0) * reward
+                     for name, reward in rewards.items())
+        # TODO: allocate weight coefficient of different reward items here!
+        if self.config["normalize_reward"]:
+            print("EcoADEnv._reward(): 'normalize_reward' yes")
+            reward = utils.lmap(reward,
+                                [self.config["collision_reward"],
+                                 self.config["high_speed_reward"] + self.config["right_lane_reward"]],
+                                [0, 1])
+        reward *= rewards['on_road_reward']  # 0 or 1
+        return reward
+
+    def _rewards(self, action: Action) -> Dict[Text, float]:
+        neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
+        lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
+            else self.vehicle.lane_index[2]
+        # Use forward speed rather than speed, see https://github.com/eleurent/highway-env/issues/268
+        forward_speed = self.vehicle.speed * np.cos(self.vehicle.heading)
+        scaled_speed = utils.lmap(forward_speed, self.config["reward_speed_range"], [0, 1])
+
+        rewards = {
+                "collision_reward": float(self.vehicle.crashed),  # True of False
+                "on_road_reward": float(self.vehicle.on_road),  # True of False
+                "right_lane_reward": lane / max(len(neighbours) - 1, 1),
+                "high_speed_reward": np.clip(scaled_speed, 0, 1)}
+        if self.config["action"]["ems_flag"]:
+            rewards.update({"EMS_reward": float(self.vehicle.EMS_reward)})
+
+        return rewards
+
+    def _is_terminated(self) -> bool:
+        """The episode is over if the ego vehicle crashed."""
+        return (self.vehicle.crashed or
+                self.config["offroad_terminal"] and not self.vehicle.on_road)
+
+    def _is_truncated(self) -> bool:
+        """The episode is truncated if the time limit is reached."""
+        return self.time >= self.config["duration"]
---
+++ highway_env/vehicle/kinematics.py
@@ -0,0 +1,275 @@
+from typing import Union, Optional, Tuple, List
+import numpy as np
+import copy
+from collections import deque
+
+from highway_env import utils
+from highway_env.road.road import Road, LaneIndex
+from highway_env.vehicle.objects import RoadObject, Obstacle, Landmark
+from highway_env.utils import Vector
+from highway_env.vehicle.agentEMS import EMS
+
+
+class Vehicle(RoadObject):
+
+    """
+    A moving vehicle on a road, and its kinematics.
+
+    The vehicle is represented by a dynamical system: a modified bicycle model.
+    Its state is propagated depending on its steering and acceleration actions.
+    """
+
+    LENGTH = 6.0
+    """ Vehicle length [m] """
+    WIDTH = 2.0
+    """ Vehicle width [m] """
+    DEFAULT_INITIAL_SPEEDS = [20, 25]
+    """ Range for random initial speeds [m/s] """
+    MAX_SPEED = 40.
+    """ Maximum reachable speed [m/s] """   # origin: [-40, 40]
+    MIN_SPEED = -40.
+    """ Minimum reachable speed [m/s] """
+    HISTORY_SIZE = 30
+    """ Length of the vehicle state history, for trajectory display"""
+
+    def __init__(self,
+                 road: Road,
+                 position: Vector,
+                 heading: float = 0,
+                 speed: float = 0,
+                 prediction_type: str = 'constant_steering'):
+        super().__init__(road, position, heading, speed)
+        self.prediction_type = prediction_type
+        self.action = {'acceleration': 0.0, 'steering': 0.0, 'engine_power': 0.0}
+        self.crashed = False
+        self.impact = None
+        self.log = []
+        self.history = deque(maxlen=self.HISTORY_SIZE)
+        # my definition
+        self.AgentEMS = EMS(w_soc=1.0, soc0=0.5, SOC_MODE='CS', abs_spd_MAX=30, abs_acc_MAX=30)
+        self.EMS_info = {}      # output to abstract.py: _info()
+        self.EMS_reward = 0     # output to ecoad_env.py: _rewards()
+        self.EMS_done = False
+
+    @classmethod
+    def create_random(cls, road: Road,
+                      speed: float = None,
+                      lane_from: Optional[str] = None,
+                      lane_to: Optional[str] = None,
+                      lane_id: Optional[int] = None,
+                      spacing: float = 1) \
+            -> "Vehicle":
+        """
+        Create a random vehicle on the road.
+
+        The lane and /or speed are chosen randomly, while longitudinal position is chosen behind the last
+        vehicle in the road with density based on the number of lanes.
+
+        :param road: the road where the vehicle is driving
+        :param speed: initial speed in [m/s]. If None, will be chosen randomly
+        :param lane_from: start node of the lane to spawn in
+        :param lane_to: end node of the lane to spawn in
+        :param lane_id: id of the lane to spawn in
+        :param spacing: ratio of spacing to the front vehicle, 1 being the default
+        :return: A vehicle with random position and/or speed
+        """
+        _from = lane_from or road.np_random.choice(list(road.network.graph.keys()))
+        _to = lane_to or road.np_random.choice(list(road.network.graph[_from].keys()))
+        _id = lane_id if lane_id is not None else road.np_random.choice(len(road.network.graph[_from][_to]))
+        lane = road.network.get_lane((_from, _to, _id))
+        if speed is None:
+            if lane.speed_limit is not None:
+                speed = road.np_random.uniform(0.7*lane.speed_limit, 0.8*lane.speed_limit)
+            else:
+                speed = road.np_random.uniform(Vehicle.DEFAULT_INITIAL_SPEEDS[0], Vehicle.DEFAULT_INITIAL_SPEEDS[1])
+        default_spacing = 12+1.0*speed
+        offset = spacing * default_spacing * np.exp(-5 / 40 * len(road.network.graph[_from][_to]))
+        x0 = np.max([lane.local_coordinates(v.position)[0] for v in road.vehicles]) \
+            if len(road.vehicles) else 3*offset
+        x0 += offset * road.np_random.uniform(0.9, 1.1)
+        v = cls(road, lane.position(x0, 0), lane.heading_at(x0), speed)
+        return v
+
+    @classmethod
+    def create_from(cls, vehicle: "Vehicle") -> "Vehicle":
+        """
+        Create a new vehicle from an existing one.
+
+        Only the vehicle dynamics are copied, other properties are default.
+
+        :param vehicle: a vehicle
+        :return: a new vehicle at the same dynamical state
+        """
+        v = cls(vehicle.road, vehicle.position, vehicle.heading, vehicle.speed)
+        if hasattr(vehicle, 'color'):
+            v.color = vehicle.color
+        return v
+
+    def act(self, action: Union[dict, str] = None) -> None:
+        """
+        Store an action to be repeated.
+
+        :param action: the input action
+        """
+        if action:
+            self.action = action
+
+    def step(self, dt: float) -> None:
+        # print("kinematics.py: Vehicle.step()")
+        """
+        Propagate the vehicle state given its actions.
+
+        Integrate a modified bicycle model with a 1st-order response on the steering wheel dynamics.
+        If the vehicle is crashed, the actions are overridden with erratic steering and braking until complete stop.
+        The vehicle's current lane is updated.
+
+        :param dt: timestep of integration of the model [s]
+        """
+        self.clip_actions()
+        delta_f = self.action['steering']
+        beta = np.arctan(1 / 2 * np.tan(delta_f))
+        v = self.speed * np.array([np.cos(self.heading + beta),
+                                   np.sin(self.heading + beta)])
+        self.position += v * dt
+        if self.impact is not None:
+            self.position += self.impact
+            self.crashed = True
+            self.impact = None
+        # self.acc_value            # update the acc-value, add it into obs-space  # not in need
+        self.heading += self.speed * np.sin(beta) / (self.LENGTH / 2) * dt
+        self.speed += self.action['acceleration'] * dt
+        self.on_state_update()
+        
+        # update EMS information here!
+        ems_obs = self.AgentEMS.execute(action=self.action['engine_power'],
+                                        car_spd=self.speed, car_acc=self.action['acceleration'])
+        self.EMS_reward = self.AgentEMS.get_reward()
+        self.EMS_info = self.AgentEMS.get_info()
+        self.EMS_done = self.AgentEMS.get_done()
+
+    def clip_actions(self) -> None:
+        if self.crashed:
+            self.action['steering'] = 0.0
+            self.action['acceleration'] = -1.0*self.speed
+            self.action['engine_power'] = 0.0
+        self.action['acceleration'] = float(self.action['acceleration'])
+        self.action['steering'] = float(self.action['steering'])
+        self.action['engine_power'] = float(self.action['engine_power'])
+        if self.speed > self.MAX_SPEED:
+            self.action['acceleration'] = min(self.action['acceleration'], 1.0 * (self.MAX_SPEED - self.speed))
+        elif self.speed < self.MIN_SPEED:
+            self.action['acceleration'] = max(self.action['acceleration'], 1.0 * (self.MIN_SPEED - self.speed))
+
+    def on_state_update(self) -> None:
+        if self.road:
+            self.lane_index = self.road.network.get_closest_lane_index(self.position, self.heading)
+            self.lane = self.road.network.get_lane(self.lane_index)
+            if self.road.record_history:
+                self.history.appendleft(self.create_from(self))
+
+    def predict_trajectory_constant_speed(self, times: np.ndarray) -> Tuple[List[np.ndarray], List[float]]:
+        if self.prediction_type == 'zero_steering':
+            action = {'acceleration': 0.0, 'steering': 0.0}
+        elif self.prediction_type == 'constant_steering':
+            action = {'acceleration': 0.0, 'steering': self.action['steering']}
+        else:
+            raise ValueError("Unknown prediction type")
+
+        dt = np.diff(np.concatenate(([0.0], times)))
+
+        positions = []
+        headings = []
+        v = copy.deepcopy(self)
+        v.act(action)
+        for t in dt:
+            v.step(t)
+            positions.append(v.position.copy())
+            headings.append(v.heading)
+        return (positions, headings)
+
+    @property
+    def velocity(self) -> np.ndarray:
+        return self.speed * self.direction  # TODO: slip angle beta should be used here
+
+    @property
+    def destination(self) -> np.ndarray:
+        if getattr(self, "route", None):
+            last_lane_index = self.route[-1]
+            last_lane_index = last_lane_index if last_lane_index[-1] is not None else (*last_lane_index[:-1], 0)
+            last_lane = self.road.network.get_lane(last_lane_index)
+            return last_lane.position(last_lane.length, 0)
+        else:
+            return self.position
+
+    @property
+    def destination_direction(self) -> np.ndarray:
+        if (self.destination != self.position).any():
+            return (self.destination - self.position) / np.linalg.norm(self.destination - self.position)
+        else:
+            return np.zeros((2,))
+
+    @property
+    def lane_offset(self) -> np.ndarray:
+        if self.lane is not None:
+            long, lat = self.lane.local_coordinates(self.position)
+            ang = self.lane.local_angle(self.heading, long)
+            return np.array([long, lat, ang])
+        else:
+            return np.zeros((3,))
+
+    def to_dict(self, origin_vehicle: "Vehicle" = None, observe_intentions: bool = True) -> dict:
+        d = {
+            'presence': 1,
+            'x': self.position[0],
+            'y': self.position[1],
+            'vx': self.velocity[0],     # from self.speed, from step()
+            'vy': self.velocity[1],
+            # where is the acceleration information?
+            'heading': self.heading,
+            'cos_h': self.direction[0],
+            'sin_h': self.direction[1],
+            'cos_d': self.destination_direction[0],
+            'sin_d': self.destination_direction[1],
+            'long_off': self.lane_offset[0],
+            'lat_off': self.lane_offset[1],
+            'ang_off': self.lane_offset[2],
+            # add EMS observation here, return actual value, normalization is in observation.py: normalize_obs()
+            'SOC': self.AgentEMS.SOC,
+            'SOH_FCS': self.AgentEMS.SOH_FCS,
+            'SOH_BAT': self.AgentEMS.SOH_batt,
+            'P_FCS': self.AgentEMS.P_FCS,
+            'P_req': self.AgentEMS.P_mot,
+            # pre-defined extra zeros for EMS
+            'extra0': 0.0,
+            'extra1': 0.0,
+            'extra2': 0.0,
+            'extra3': 0.0,
+        }
+        if not observe_intentions:
+            d["cos_d"] = d["sin_d"] = 0
+        if origin_vehicle:
+            origin_dict = origin_vehicle.to_dict()
+            for key in ['x', 'y', 'vx', 'vy']:
+                d[key] -= origin_dict[key]
+        return d
+
+    def __str__(self):
+        return "{} #{}: {}".format(self.__class__.__name__, id(self) % 1000, self.position)
+
+    def __repr__(self):
+        return self.__str__()
+    
+    @property       # called in ecoad_env.py L107
+    def total_demand_power(self) -> float:
+        car_spd = self.speed
+        car_acc = self.action['acceleration']
+        
+        _, _, P_axle = self.AgentEMS.FCHEV.T_W_axle(car_spd, car_acc)
+        P_axle /= 1000
+        print('\n-----here is in Vehicle.kinematics.total_demand_power,'
+              '\n     car_spd: %.3f m/s, car_acc: %.3f m/s2, P_axle: %.1f kW,' % (car_spd, car_acc, P_axle))
+        print('     action: ', self.action, '-----')
+        return P_axle
+        
+        
+        
