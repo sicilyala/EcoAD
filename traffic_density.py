b14c9cb87619d39d6a6dbf0c272fdcdbcca20350
weiqi <cwq@linux.service>
2024-04-06 16:25:18 +0800

    vehicle density

--- traffic_density.py
+++ traffic_density.py
@@ -11,16 +11,18 @@ def traffic_density(vehicles_density, speed=20):
     density = 1 + 1000 / (offset * 1.0)
     return density
 
-vehicles_density = [1, 1.5, 2, 2.5, 3]
+vehicles_density = [1, 1.5, 2, 2.25, 2.5, 3]
 traffic_density_list = {}
 for density in vehicles_density:
     traffic_density_list[density] = traffic_density(density) 
 print(traffic_density_list)
 
 """
-vehicle_density = [1, 1.5, 2, 2.5, 3]
+vehicle_density = [1, 1.5, 2, 2.5, 2.25, 3]
 vehicle distance: m
 {1: 21.99325692131111, 1.5: 14.662171280874073, 2: 10.996628460655556, 2.5: 8.797302768524444, 3: 7.331085640437037} 
 traffic density: v/km/ln
 {1: 46.46848170681879, 1.5: 69.2027225602282, 2: 91.93696341363759, 2.5: 114.67120426704697, 3: 137.4054451204564}
+{1: 46.46848170681879, 1.5: 69.2027225602282, 2: 91.93696341363759, 
+    2.25: 103.30408384034229, 2.5: 114.67120426704697, 3: 137.4054451204564}
 """
